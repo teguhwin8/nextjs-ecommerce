d975c84de79d0d3c4bc20bfdf8cffbb9b3ac2940
Teguh Widodo <teguhwin8@gmail.com>
2022-04-23 02:15:03 +0700

    Navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
+import Link from "next/link";
+import { AiOutlineShopping } from "react-icons/ai";
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+	return <div className="navbar-container">
+    <p className="logo">
+      <Link href="/">JSM Headphones</Link>
+    </p>
 
-export default Navbar
+    <button type="button" className="cart-icon" onClick="" >
+      <AiOutlineShopping />
+      <span className="cart-item-qty">1</span>
+    </button>
+  </div>;
+};
+
+export default Navbar;
